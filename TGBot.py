c3e4c4be64da20ca7b37d336816c542e8479b4cb
Сергей Шиляев <seraregreg@gmail.com>
2024-06-12 21:23:04 +1000

    Немного обновил бота

--- TGBot.py
+++ TGBot.py
@@ -1,20 +1,33 @@
 #!/usr/bin/python
 # -*- coding: cp1251  -*-
+from re import T
 from PreProcessPhoto import PreProcessPhoto
 import telebot;
+from telebot import types
 import cv2;
 import threading 
 import os 
 bot = telebot.TeleBot('6817178987:AAGbWfjbW9_GDZxSmQO-oloJPrj6_yHQxzM');
-@bot.message_handler(content_types=['text'])
+
+@bot.message_handler(content_types=['text'])  
 def get_text_messages(message):
-    if message.text == "/start":
-        bot.send_message(message.from_user.id, "Обрезать фото и")
-    if message.text == "/start":
-        bot.send_message(message.from_user.id, "Привет, отправь фото")
+    PreProcess = False
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn1 = types.KeyboardButton("Обрезка фото")
+    btn2 = types.KeyboardButton("Распознавание текста")
+    markup.add(btn1, btn2)
+    if message.text == "Обрезка фото":
+        bot.send_message(message.from_user.id, "Отправьте фото для обрезки P.S. Оберзаем фото документов на фоне другого цвета..")
+        img = open("example.jpg", 'rb')
+        bot.send_photo(message.from_user.id, img, caption='Например,')
+        PreProcess = True
+    if message.text == "Распознавание текста":
+        bot.send_message(message.from_user.id, "Отправьте фото для распознавания")
+        PreProcess = False
+        
 @bot.message_handler(content_types=['photo'])
 def photo(message):
-    ph = threading.Thread(target = Work, args = (message,))
+    ph = threading.Thread(target = Work, args = (message,)) # Для реализации многопоточности 2 метода.
     ph.start()
 def Work(message):
     fileID = message.photo[-1].file_id   
@@ -30,6 +43,7 @@ def Work(message):
         img.close()
         print('Отправлено ' + str(message.from_user.id))
         os.remove("temp" + str(message.from_user.id) +".jpg")
+print("Start...")
 bot.polling(none_stop=True, interval=0)
 
  
