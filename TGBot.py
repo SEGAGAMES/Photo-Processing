11164aac6e0e9e5c9eb2919544ad2bbad7bd1cd9
Сергей Шиляев <seraregreg@gmail.com>
2024-06-12 22:20:23 +1000

    Немного Бота и ТУ ДУ для Вовы

--- TGBot.py
+++ TGBot.py
@@ -8,27 +8,37 @@ import threading
 import os 
 
 bot = telebot.TeleBot('6817178987:AAGbWfjbW9_GDZxSmQO-oloJPrj6_yHQxzM');
-PreProcess = False
+PreProcess = True
+
 @bot.message_handler(content_types=['text'])  
 def get_text_messages(message):
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    btn1 = types.KeyboardButton("Обрезка фото")
-    btn2 = types.KeyboardButton("Распознавание текста")
-    markup.add(btn1, btn2)
+    
     if message.text == "Обрезка фото":
+        print("text with: " + str(message.from_user.id))
         bot.send_message(message.from_user.id, "Отправьте фото для обрезки P.S. Оберзаем фото документов на фоне другого цвета..")
         img = open("example.jpg", 'rb')
         bot.send_photo(message.from_user.id, img, caption='Например,')
         PreProcess = True
-    if message.text == "Распознавание текста":
+    elif message.text == "Распознавание текста":
+        print("text with: " + str(message.from_user.id))
         bot.send_message(message.from_user.id, "Отправьте фото для распознавания")
         PreProcess = False
+        print()
+    else:
+        print("start with: " + str(message.from_user.id))
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        btn1 = types.KeyboardButton("Обрезка фото")
+        btn2 = types.KeyboardButton("Распознавание текста")
+        markup.add(btn1, btn2)
+        bot.send_message(message.chat.id, text="Выбери опцию", reply_markup=markup)
         
 @bot.message_handler(content_types=['photo'])
 def photo(message):
     if PreProcess == True:
         ph = threading.Thread(target = Work, args = (message,)) # Для реализации многопоточности 2 метода.
         ph.start()
+    else:
+         bot.send_message(message.from_user.id, "еще не робит..")
 def Work(message):
     fileID = message.photo[-1].file_id   
     file_info = bot.get_file(fileID)
